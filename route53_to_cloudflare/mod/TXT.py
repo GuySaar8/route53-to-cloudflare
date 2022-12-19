6f7e33339240ff0b43776b2974d6b2bb09af4600
GuySaar8 <77405986+GuySaar8@users.noreply.github.com>
2022-12-19 19:46:16 +0200

    Update TXT.py

--- route53_to_cloudflare/mod/TXT.py
+++ route53_to_cloudflare/mod/TXT.py
@@ -20,24 +20,24 @@ def set_TXT_value(record_values):
     value8 = ""
     value9 = ""
     value10 = ""
-    if (len(record_values)) == 1:
+    if (len(record_values)) > 0:
         value1 = fix_TXT_Value(record_values[0]['Value'].replace('"', ''))
-    elif (len(record_values)) == 2:
+    if (len(record_values)) > 1:
         value2 = fix_TXT_Value(record_values[1]['Value'].replace('"', ''))
-    elif (len(record_values)) == 3:
+    if (len(record_values)) >2 :
         value3 = fix_TXT_Value(record_values[2]['Value'].replace('"', ''))
-    elif (len(record_values)) == 4:
+    if (len(record_values)) > 3:
         value4 = fix_TXT_Value(record_values[3]['Value'].replace('"', ''))
-    elif (len(record_values)) == 5:
+    if (len(record_values)) > 4:
         value5 = fix_TXT_Value(record_values[4]['Value'].replace('"', ''))
-    elif (len(record_values)) == 6:
+    if (len(record_values)) > 5:
         value6 = fix_TXT_Value(record_values[5]['Value'].replace('"', ''))
-    elif (len(record_values)) == 7:
+    if (len(record_values)) > 6:
         value7 = fix_TXT_Value(record_values[6]['Value'].replace('"', ''))
-    elif (len(record_values)) == 8:
+    if (len(record_values)) > 7:
         value8 = fix_TXT_Value(record_values[7]['Value'].replace('"', ''))
-    elif (len(record_values)) == 9:
+    if (len(record_values)) > 8:
         value9 = fix_TXT_Value(record_values[8]['Value'].replace('"', ''))
-    elif (len(record_values)) == 10:
+    if (len(record_values)) == 10:
         value10 = record_values[9]['Value'].replace('"', '')
     return value1, value2, value3, value4, value5, value6, value7, value8, value9, value10
